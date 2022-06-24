4308ee54066bdd4e565353bc7f565fa233b65a32
gauravo7 <gaurav@o7services.com>
2022-06-24 12:41:03 +0530

    Booostrap and Map in React
    How to Use Loop IN JSX
    What is JSX
    Map Function and Make Dynamic Element using Map
    Attach Bootstrap File in React Project and Use the Bootclass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import About from './About';
 import Employee from './Employee';
 import AddEmployee from './Employee/AddEmployee';
 function App() {
+  let students = [{
+                name:"Deva",
+                Course:"Btech",
+                age:23
+              },{
+                name:"Anjali",
+                Course:"MCA",
+                age:45
+              },{
+                name:"James",
+                Course:"BVOC",
+                age:29
+              },{
+                name:"Sharma",
+                Course:"BVOC",
+                age:33
+              }];
   return (
       <div>
         <About/>
@@ -10,6 +27,16 @@ function App() {
         <Employee name="Anita" code="o7s/004"/>
         <Employee name="Daman" code="o7s/010"/>
         <AddEmployee />
+
+        <br/>
+        <ul>
+          {students.map((element,index )=>(
+            <li className='text-danger' key={index+1}>
+              Name: {element.name} - Course: {element.Course} - Age:{element.age}
+            </li>
+          )) }
+          
+        </ul>
       </div>      
   );
 }
--- src/Employee/AddEmployee.js
+++ src/Employee/AddEmployee.js
@@ -4,9 +4,12 @@ export default function AddEmployee(){
     
     const [ename,setEname] = useState("Gaurav");
     const [ecode,setEcode] = useState("o7s"); 
-    const showData = ()=>{
+    const showData = (e)=>{
+        e.preventDefault();
         console.log("Name is ",ename)
         console.log("Code is ",ecode) 
+        setEcode("")
+        setEname("")
     }
     const onsetName = (e) =>{
         setEname(e.target.value)
@@ -14,19 +17,21 @@ export default function AddEmployee(){
     const onsetCode = (e)=> setEcode(e.target.value) 
     return(
         <div>
-            Enter EMP Name:{ename}<br/>
-            <input 
-                type="text" 
-                name="" 
-                onChange={onsetName} 
-                value={ename}
-            />
-            <br/>
-            Enter EMP Code:<br/>
-            <input type="text" name="ecode" value={ecode}
-            onChange={onsetCode}
-            />
-            <input type="submit" onClick={showData} />
+            <form onSubmit={showData}>
+                Enter EMP Name:{ename}<br/>
+                <input 
+                    type="text" 
+                    name="" 
+                    onChange={onsetName} 
+                    value={ename}
+                />
+                <br/>
+                Enter EMP Code:<br/>
+                <input type="text" name="ecode" value={ecode}
+                onChange={onsetCode}
+                />
+                <input type="submit"  />
+            </form>
         </div>
     );
 }
